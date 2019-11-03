3fb6f4d82690ac534c62bcc52a86a5186c2b3dec
clhuang224 <clhuang224@gmail.com>
2019-11-03 21:04:46 +0800

    Update index

--- js/all.js
+++ js/all.js
@@ -6,7 +6,6 @@ let data = [
         articleURL: 'https://clhuang224.github.io/TechBlog/2019/11/03/20191103-hex-jsunderground-01/',
         workURL: './01_MultiplicationChart/',
         codeURL: 'https://github.com/clhuang224/JavaScriptUnderground/tree/master/01_MultiplicationChart',
-        imageURL: 'img/01.png',
     }
 ];
 
@@ -25,11 +24,11 @@ for (let i = 0; i < data.length; i++) {
             <div class="content">
                 <h2 class="title">${data[i].title}</h2>
                 <ul class="links">
-                    <li><a href="${data[i].articleURL}" target="_blank">挑戰紀錄</a></li>
-                    <li><a href="${data[i].workURL}" target="_blank">作品網址</a></li>
+                    <li><a href="${data[i].workURL}" target="_blank">作品連結</a></li>
                     <li><a href="${data[i].codeURL}" target="_blank">原始碼</a></li>
+                    <li><a href="${data[i].articleURL}" target="_blank">紀錄文章</a></li>
                 </ul>
-                <img class="img" src="${data[i].imageURL}">
+                <iframe class="iframe" src="${data[i].workURL+'index.html'}"></iframe>
             </div>
         </div>`;
 }
@@ -50,3 +49,5 @@ else {
 }
 document.querySelector(`.main #list-tab a[href="#${href.toLowerCase()}"]`).parentElement.classList.add('active');
 document.querySelector(`.main .tab-content .tab-pane[id="${href.toLowerCase()}"]`).classList.add('active');
+
+document.querySelector('.content .iframe').height = window.innerHeight - 234;
